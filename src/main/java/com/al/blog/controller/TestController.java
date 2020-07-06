4a66f88251dc6bf8fea6be6a1943966b518c034a
Aaron <1546848781@qq.com>
2020-07-06 18:25:51 +0800

    统一返回结果+enum ResponseState

--- src/main/java/com/al/blog/controller/TestController.java
+++ src/main/java/com/al/blog/controller/TestController.java
@@ -1,21 +1,27 @@
 package com.al.blog.controller;
 
-import com.al.blog.pojo.User;
-import org.springframework.web.bind.annotation.*;
+import com.al.blog.response.ResponseResult;
+import com.al.blog.response.ResponseState;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/test")
 public class TestController {
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
 
-    public String helloWorld() {
+    public ResponseResult helloWorld() {
         System.out.println("hello world");
-        return "hello";
+        ResponseResult responseResult = new ResponseResult(ResponseState.SUCCESS);
+        responseResult.setData("hello");
+
+        return responseResult;
     }
     
-    @GetMapping("test-json")
+    /*@GetMapping("test-json")
     public User testJson() {
         User user = new User("al", 20, "male");
         return user;
-    }
+    }*/
 }
