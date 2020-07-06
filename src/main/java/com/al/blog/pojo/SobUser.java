9e37da06ada28ee44a53e41628855c004f3f35dd
Aaron <1546848781@qq.com>
2020-07-06 16:53:01 +0800

    手写bean类

---
+++ src/main/java/com/al/blog/pojo/SobUser.java
@@ -0,0 +1,145 @@
+package com.al.blog.pojo;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Date;
+
+@Table(name = "tb_user")
+@Entity
+public class SobUser {
+    @Id
+    @Column(name = "id")
+    private String id;
+
+    @Column(name = "user_name")
+    private String userName;
+
+    @Column(name = "password")
+    private String password;
+
+
+    @Column(name = "roles")
+    private String roles;
+
+    @Column(name = "avatar")
+    private String avatar;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "sign")
+    private String sign;
+
+    @Column(name = "state")
+    private String state;
+
+    @Column(name = "reg_ip")
+    private String reg_ip;
+
+    @Column(name = "login_ip")
+    private String login_ip;
+
+    @Column(name = "create_time")
+    private Date createTime;
+
+    @Column(name = "update_time")
+    private Date updateTime;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRoles() {
+        return roles;
+    }
+
+    public void setRoles(String roles) {
+        this.roles = roles;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSign() {
+        return sign;
+    }
+
+    public void setSign(String sign) {
+        this.sign = sign;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getReg_ip() {
+        return reg_ip;
+    }
+
+    public void setReg_ip(String reg_ip) {
+        this.reg_ip = reg_ip;
+    }
+
+    public String getLogin_ip() {
+        return login_ip;
+    }
+
+    public void setLogin_ip(String login_ip) {
+        this.login_ip = login_ip;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+}
